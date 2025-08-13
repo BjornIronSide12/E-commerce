36c9f458b61f2642e1157c0516e2b17dd832f5c0
Raghvendra <raghvendrasonarikar@gmail.com>
2025-08-13 17:22:58 +0530

    adding repo layer

--- productcatalog/src/main/java/com/ecommerce/productcatalog/models/BaseModel.java
+++ productcatalog/src/main/java/com/ecommerce/productcatalog/models/BaseModel.java
@@ -1,6 +1,16 @@
 package com.ecommerce.productcatalog.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import java.util.UUID;
+
+@MappedSuperclass
 public class BaseModel {
 
-    private Integer id;
+    @Id
+    @GeneratedValue(generator = "myGenerator")
+    @Column(name = "id")
+    private UUID uuid;
 }
